0e63223dec454bafc5ea58dc17a44c8d88d60c3c
SamuraiBUPT <2781374337@qq.com>
2023-02-23 22:16:34 +0800

    规定cpp版本& 修改main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <vector>
 
 int main() {
     std::cout << "This is our Operating System demo." << std::endl;
+    std::cout << "This gonna be a brilliant project." << std::endl;
     return 0;
 }
