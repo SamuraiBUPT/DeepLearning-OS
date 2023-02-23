c4d4e1ab836df2c4f83e483feece49c34c85433d
SamuraiBUPT <2781374337@qq.com>
2023-02-23 22:49:32 +0800

    CMake开发包尝试

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-#include <vector>
+#include "pkg_1/pkg_1.h"
 
 int main() {
     std::cout << "This is our Operating System demo." << std::endl;
     std::cout << "This gonna be a brilliant project." << std::endl;
+    init();
     return 0;
 }
