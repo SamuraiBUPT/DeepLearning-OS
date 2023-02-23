d3d2d945a515d60944d9e7240cddcd045d3769b1
SamuraiBUPT <2781374337@qq.com>
2023-02-23 22:14:02 +0800

    规定cpp版本

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "This is our Operating System demo!" << std::endl;
+    std::cout << "This is our Operating System demo." << std::endl;
     return 0;
 }
